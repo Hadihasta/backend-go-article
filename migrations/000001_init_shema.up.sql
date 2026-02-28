b19d2442fba4867bce989b8f2461f07bc8bb2447
hadiasta <hijrihadi@gmail.com>
2026-02-28 15:40:07 +0700

    feat : add migration file

---
+++ migrations/000001_init_shema.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE
+    posts (
+        id int AUTO_INCREMENT PRIMARY KEY,
+        title varchar(200),
+        content text,
+        category varchar(100),
+        created_at timestamp,
+        updated_at timestamp,
+        status varchar(100)
+    );
